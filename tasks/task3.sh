8a632609742a22f227ee0b54d99761d8c6257873
Brijesh Kumar <39380117+KoruptTinker@users.noreply.github.com>
2024-10-10 17:16:40 +0000

    Add: Task3.sh

---
+++ tasks/task3.sh
@@ -0,0 +1,1 @@
+cat titanic.csv | gawk -F, '{print $6" "$7" "$3" "$13" "$4" "$5}' | grep -E '[0-9]+\s[2]\sS' | sed 's/female/F/' | sed 's/male/M/' | awk '{ sum += $2; n++; print $4" "$5" "$6" "$7" "$1" "$2" "$3} END { if (n > 0) print "Average Age: "sum/n; }'
